8d5fc5195a22b446c491fa07193d8f01569eea7e
ssatz <ssatz@users.noreply.github.com>
2020-05-23 08:34:38 +0000

    Fix styling

--- src/JoloApiServiceProvider.php
+++ src/JoloApiServiceProvider.php
@@ -4,20 +4,16 @@
  *  *  * Copyright (C) Optimo Technologies - All Rights Reserved
  *  *  * Unauthorized copying of this file, via any medium is strictly prohibited
  *  *  * Proprietary and confidential
- *  *  * Written by Sathish Kumar(satz) <sathish.thi@gmail.com> ManiKandan <smanikandanit@gmail.com>
- *  *
- *
+ *  *  * Written by Sathish Kumar(satz) <sathish.thi@gmail.com> ManiKandan <smanikandanit@gmail.com>.
  */
 
 namespace OptimoApps\JoloApi;
-
 
 use Illuminate\Support\ServiceProvider;
 use OptimoApps\JoloApi\Exceptions\InvalidConfiguration;
 
 /**
- * Class JoloApiServiceProvider
- * @package Optimo\JoloApi
+ * Class JoloApiServiceProvider.
  */
 class JoloApiServiceProvider extends ServiceProvider
 {
@@ -28,7 +24,7 @@ class JoloApiServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $configPath = __DIR__ . '/../config/jolo-api.php';
+        $configPath = __DIR__.'/../config/jolo-api.php';
         $this->publishes([
             $configPath => config_path('courier.php'),
         ]);
@@ -44,13 +40,13 @@ class JoloApiServiceProvider extends ServiceProvider
         $this->app->bind(JoloApi::class, function () {
             $config = config('jolo-api');
             $this->checkInvalidConfiguration($config);
+
             return JoloApiFactory::createForConfig($config);
         });
         $this->app->alias(JoloApi::class, 'jolo-api');
     }
 
     /**
-     * @param array|null $config
      * @throws InvalidConfiguration
      */
     protected function checkInvalidConfiguration(array $config = null)
--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -4,9 +4,7 @@
  *  *  * Copyright (C) Optimo Technologies - All Rights Reserved
  *  *  * Unauthorized copying of this file, via any medium is strictly prohibited
  *  *  * Proprietary and confidential
- *  *  * Written by Sathish Kumar(satz) <sathish.thi@gmail.com>ManiKandan<smanikandanit@gmail.com >
- *  *
- *
+ *  *  * Written by Sathish Kumar(satz) <sathish.thi@gmail.com>ManiKandan<smanikandanit@gmail.com >.
  */
 
 namespace OptimoApps\JoloApi\Test\Integration;
@@ -17,7 +15,7 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 abstract class TestCase extends OrchestraTestCase
 {
-    public function setUp():void
+    public function setUp(): void
     {
         parent::setUp();
     }
